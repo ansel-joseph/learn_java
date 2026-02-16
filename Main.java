2998660f1482a9be1e9fe0065b4698a82fefd1f4
Ansel Joseph <anseljoseph2509@gmail.com>
2026-02-16 22:17:08 +0530

    add: constructors

--- Main.java
+++ Main.java
@@ -1,23 +1,30 @@
-class Student{
-    int rollno;
-    String name;
-    int marks;
-    Student(int rollno, String name, int marks){
-        this.rollno=rollno;
-        this.name=name;
-        this.marks=marks;
+import java.util.Scanner;
+class car{
+    String model;
+    String company;
+    double price;
+    car(){
+        System.out.println("Default constructor called");
+    }       
+    car(String model, String company, double price){
+        this.model=model;
+        this.company=company;
+        this.price=price;
     }
     void display(){
-        System.out.println("Rollno: "+ rollno);
-        System.out.println("Name: "+ name);
-        System.out.println("Marks: "+ marks);
+        System.out.println("Model: "+model);
+        System.out.println("Company: "+company);
+        System.out.println("Price: "+price);
     }
+
 }
 public class Main{
     public static void main(String[] args){
-        Student s1 = new Student(001, "Sarthak", 78);
-        s1.display();
+        car c1 = new car("Cybertruck","Tesla", 6000000.0);
+        c1.display();
+        car c2=new car();
+        c2.display();
+
 
     }
-
 }
