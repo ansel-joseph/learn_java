144fc9589c75b72adb8d457f850251eb5a1e8471
Ansel Joseph <anseljoseph2509@gmail.com>
2026-02-16 18:37:18 +0530

    add: simple parameter

--- Main.java
+++ Main.java
@@ -1,23 +1,23 @@
-class ElectronicDevice{
-    void powerOn(){
-        System.out.println("Device is powering on");
+class Student{
+    int rollno;
+    String name;
+    int marks;
+    Student(int rollno, String name, int marks){
+        this.rollno=rollno;
+        this.name=name;
+        this.marks=marks;
     }
-}
-class Computer extends ElectronicDevice{
-    void compute(){
-        System.out.println("Computer is proecessing data");
-    }
-}
-class Laptop extends Computer{
-    void portable(){
-        System.out.println("Laptop is portable");
+    void display(){
+        System.out.println("Rollno: "+ rollno);
+        System.out.println("Name: "+ name);
+        System.out.println("Marks: "+ marks);
     }
 }
 public class Main{
-    public static void main (String[] args){
-        Laptop l = new Laptop();
-        l.powerOn();
-        l.compute();
-        l.portable();
+    public static void main(String[] args){
+        Student s1 = new Student(001, "Sarthak", 78);
+        s1.display();
+
     }
+
 }
