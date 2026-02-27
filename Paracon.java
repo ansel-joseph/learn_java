4ae66bae84536253c65e45b0632acb9e0b442369
Ansel Joseph <anseljoseph2509@gmail.com>
2026-02-27 12:52:45 +0530

    add: basic default constructor

--- Paracon.java
+++ Paracon.java
@@ -1,15 +1,14 @@
 class parameter{
     int id;
     String name;
-    parameter(int id, String name){
-        this.id=id;
-        this.name=name;
+    parameter(){
+
     }
 }
 
 public class Paracon{
     public static void main(String [] args){
-        parameter p = new parameter(23, "Ansel");
+        parameter p = new parameter();
         System.out.println(p.id);
         System.out.println(p.name);
     }
